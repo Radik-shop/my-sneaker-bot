a898bad5db1177f9b30e07f9559f6099a9e6c48b
Radik-shop <tumasevskijr@gmail.com>
2026-04-09 21:05:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive() # Этот вызов "оживит" сервер для Render
+
 import telebot
 from telebot import types
 
